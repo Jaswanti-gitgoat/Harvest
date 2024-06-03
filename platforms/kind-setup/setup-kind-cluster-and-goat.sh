8c01e933c823f6a1afc2baf97beae5d1fe48bb34
za <zakiakhmad@gmail.com>
2024-06-03 13:35:12 +0700

    Updated GCP docs
    on how to deploy kubernetes-goat.

--- platforms/kind-setup/setup-kind-cluster-and-goat.sh
+++ platforms/kind-setup/setup-kind-cluster-and-goat.sh
@@ -29,4 +29,4 @@ kind create cluster --config kind-cluster-setup.yaml --name kubernetes-goat-clus
 cd ../..
 
 # Setup GOAT exposing host Docker socket:
-sh setup-kubernetes-goat.sh
+sed 's/scenarios\/health-check\/deployment.yaml/scenarios\/health-check\/deployment-kind.yaml/' setup-kubernetes-goat.sh | sh
